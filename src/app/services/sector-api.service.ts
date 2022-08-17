a6cbd71b07ca0f608b0fe8ed6576fbad01bef9ba
Jelly <j_andrespuk@hotmail.com>
2022-08-17 13:07:23 +0300

    - sector-api.service

---
+++ src/app/services/sector-api.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {API_URL} from "../app.constants";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SectorApiService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  doesUserExist(username: string): Observable<boolean> {
+    return this.httpClient.get<boolean>(API_URL + `/users/exists?userName=${username}`)
+  }
+}
